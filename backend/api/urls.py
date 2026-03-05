3cc20e976306383f3211e32b9d224c18d2397489
Abdelrahman Hamed <h70abdelrahmanelsayed@gmail.com>
2026-03-05 03:59:24 +0200

    finish the Favorites app

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,4 +2,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('accounts/', include('api.accounts_api.urls')),
+    path('properties/', include('api.properties_api.urls')),
+    path('bookings/', include('api.bookings_api.urls')),
+    path('favorites/', include('api.favorites_api.urls')),
 ]
